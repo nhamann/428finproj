25ab80c7afe2049e2b080ff0fce8149552fcc715
j <skronkasaurus@gmail.com>
2009-05-04 08:02:34 -0500

    final version.

---
+++ plot4code/add8.v
@@ -0,0 +1,10 @@
+module add8(a, b, cin, sum, cout);
+  output reg [7:0] sum;
+  output reg cout;
+  input [7:0] a;
+  input [7:0] b;
+  input cin;
+
+  always @(a, b, cin)
+    {cout, sum} = a + b + cin;
+endmodule
